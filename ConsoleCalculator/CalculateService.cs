7430bd74c7796236dda2f5e6ccf0e2a534c5200e
Loc Nguyen <loc.nguyen@vServer.com>
2019-06-13 07:33:35 +0700

    First commit

---
+++ ConsoleCalculator/CalculateService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleCalculator
+{
+    internal class CalculateService
+    {
+        public CalculateService()
+        {
+        }
+
+        internal double Add(List<double> listNumbers)
+        {
+            double result = listNumbers[0];
+
+            for (int i = 1; i < listNumbers.Count; i++) {
+                result += listNumbers[i];
+            }
+            return result;
+        }
+
+        internal double Sub(List<double> listNumbers)
+        {
+            double result = listNumbers[0];
+
+            for (int i = 1; i < listNumbers.Count; i++)
+            {
+                result -= listNumbers[i];
+            }
+            return result;
+        }
+
+        internal double Mul(List<double> listNumbers)
+        {
+            double result = listNumbers[0];
+
+            for (int i = 1; i < listNumbers.Count; i++)
+            {
+                result *= listNumbers[i];
+            }
+            return result;
+        }
+
+        internal double Div(List<double> listNumbers)
+        {
+            double result = listNumbers[0];
+
+            for (int i = 1; i < listNumbers.Count; i++)
+            {
+                if (listNumbers[i] == 0)
+                {
+
+                }
+                result /= listNumbers[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ui = new UserInterface();
+            var service = new CalculateService();
+            double result = 0;
+
+            ui.ShowMenuOperator();
+            ui.ShowInputNumbers();
+            char operators = ui.GetOperator();
+            List<double> listNumbers = ui.GetListInputNumbers();
+            if(listNumbers.Count < 2)
+            {
+                ui.Inform("Not enough numbers to calculate!");
+            }
+            else
+            {
+                switch (operators)
+                {
+                    case '+':
+                        result = service.Add(listNumbers);
+                        ui.ShowResult(result, operators, listNumbers);
+                        break;
+                    case '-':
+                        result = service.Sub(listNumbers);
+                        ui.ShowResult(result, operators, listNumbers);
+                        break;
+                    case '*':
+                        result = service.Mul(listNumbers);
+                        ui.ShowResult(result, operators, listNumbers);
+                        break;
+                    case '/':
+                        result = service.Div(listNumbers);
+                        ui.ShowResult(result, operators, listNumbers);
+                        break;
+                }
+            }
+        }
+    }
+}
